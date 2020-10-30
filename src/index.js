5207282cb6ebc92a8b0f55443e4a31c17f1a4b66
dillonstreator <dillonstreator@gmail.com>
2020-10-29 20:01:02 -0500

    fix iteration count bug and prevent number overflow with guard

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 const _Cycleable_ = (() => {
     const attachments = {};
+    const NUM_ITERATIONS_BEFORE_REMOVAL = 3;
     const newElem = ({ type, className, text }) => {
         const element = document.createElement(type);
         element.classList.add(className);
@@ -21,7 +22,9 @@ const _Cycleable_ = (() => {
                 initialDelay = parseInt(el.getAttribute(delayAttr));
             } catch (e) { initialDelay=0; }
     
-            let idx = its = 0;
+            let idx = 0,
+                its = 0,
+                shouldRemove = false;
             el.appendChild(newElem({ type: 'span', text: items[idx] }));
             setTimeout(() => {
                 setInterval(() => {
@@ -31,7 +34,8 @@ const _Cycleable_ = (() => {
                     el.firstChild.classList.add(outClass);
                     el.insertBefore(newElem({ type: 'span', className: inClass, text: items[idx] }), el.firstChild)
                     el.style.width = window.getComputedStyle(el.firstChild).width;
-                    if (++its >= 3) el.removeChild(el.lastChild);
+                    if (!shouldRemove && ++its >= NUM_ITERATIONS_BEFORE_REMOVAL) shouldRemove = true
+                    if (shouldRemove) el.removeChild(el.lastChild);
     
                 }, timeout);
             }, initialDelay);
